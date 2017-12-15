9dd0b3870f351e7df50b46639f4ab42c6be7ec41
manuelgeek <emashmagak@appslab.co.ke>
2017-12-15 17:03:38 +0300

    initial commit, nothing much done, not yet tested

---
+++ database/migrations/2017_12_15_130959_create_transactions_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('TransID')->unique();
+            $table->string('TransAmount');
+            $table->string('BillRefNumber')->nullable();
+            $table->string('InvoiceNumber')->nullable();
+            $table->string('OrgAccountBalance')->nullable();
+            $table->string('ThirdPartyTransID')->nullable();
+            $table->string('MSISDN')->nullable();
+            $table->string('FirstName')->nullable();
+            $table->string('MiddleName')->nullable();
+            $table->string('LastName')->nullable();
+            $table->boolean('valid')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
